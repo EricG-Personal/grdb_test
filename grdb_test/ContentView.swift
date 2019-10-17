e2c25546c792ea5fbce9b9e7772a9f1deda0efff
ericg <ericgorr@gmail.com>
2019-10-16 20:08:35 -0400

    Initial Commit

---
+++ grdb_test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  grdb_test
+//
+//  Created by ericg on 10/16/19.
+//  Copyright © 2019 ericg. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ grdb_testTests/grdb_testTests.swift
@@ -0,0 +1,34 @@
+//
+//  grdb_testTests.swift
+//  grdb_testTests
+//
+//  Created by ericg on 10/16/19.
+//  Copyright © 2019 ericg. All rights reserved.
+//
+
+import XCTest
+@testable import grdb_test
+
+class grdb_testTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
